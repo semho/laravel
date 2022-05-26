93afb10f82b455d2c9e1e7b56890bac20f351648
Semyon Khokhalev <semho@yandex.ru>
2022-05-26 04:36:24 +0000

    fix trable TagsContoller

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -8,7 +8,7 @@ class TagsController extends Controller
 {
     public function index(Tag $tag)
     {
-        $articles = $tag->articles()->with('tags')->get();
+        $articles = $tag->articles()->with('tags')->orderByDesc('id')->simplePaginate(10);
 
         return view('articles.index', compact('articles'));
     }
